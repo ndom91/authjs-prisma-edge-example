adcfc40bd977422578a60e362ae4c91af85c7fad
ndom91 <yo@ndo.dev>
2024-04-04 12:19:02 +0200

    fix: readd next.config experiments

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack(config) {
+    config.experiments = {
+      asyncWebAssembly = true,
+      layers: true
+    }
 
+    return config
+  },
+}
 module.exports = nextConfig
